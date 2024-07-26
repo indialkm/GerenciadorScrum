9cd7cf6d7849d489af23a95a13c29c59181ddb23
Ingrid Alkimim <129517270+indialkm@users.noreply.github.com>
2024-07-25 22:06:44 -0300

    Update BacklogProduto.java

--- src/model/BacklogProduto.java
+++ src/model/BacklogProduto.java
@@ -4,4 +4,5 @@ public class BacklogProduto {
 	private Integer id;
 	private String status;
 	private Integer idHistoriaUsuario; // list EstoriaDeUsuario estoriaDeUsuario
+	//https://blog.myscrumhalf.com/o-quadro-de-tarefas-no-scrum/
 }
